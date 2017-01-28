97c7c58a96b4566e3476fbdf0e3500bf21568b60
Cara Heacock <caraheacock@gmail.com>
2017-01-28 14:43:35 -0600

    Adds plugin

---
+++ wp-content/plugins/the-unarmed-truth-functionality/the-unarmed-truth-functionality.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name:         The Unarmed Truth Functionality
+ * Plugin URI:          https://github.com/caraheacock/the-unarmed-truth
+ * Description:         Functionality for The Unarmed Truth site
+ * Version:             1.0.0
+ * Author:              Cara Heacock
+ * Author URI:          http://caraheacock.com
+ * License:             GNU General Public License v2 or later
+ * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least:   4.7
+ * Tested up to:        4.7
+ * Text Domain:         unarmed-truth-func
+ */
+
+if (!defined('ABSPATH')) exit;
+
+/* Set global variables */
+define('UTRUF_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('UTRUF_PLUGIN_URL', plugins_url('/', __FILE__));
+
+?>
